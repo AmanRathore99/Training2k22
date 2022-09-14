3fe2d3ad7df78efb4c7e5bf3fd85f003e1bc0cb7
AmanRathore99 <38449488+AmanRathore99@users.noreply.github.com>
2022-09-14 15:57:20 +0530

    Delete

--- MySql/LoadEmp.sql
+++
@@ -1,4 +0,0 @@
-insert into my_employee values(2,'Dancs','Betty','bdancs',860);
-insert into my_employee values(3,'Biri','Ben','bbiri',1100);
-insert into my_employee values(4,'Newman','Chad','cnewman',750);
-insert into MY_EMPLOYEE values(5,'Ropeburn','Audry','aropeburn',1550);
